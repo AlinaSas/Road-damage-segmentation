0befd04e6eae43acb657182ea6ab67cf91f8bd63
Alina Sergeeva <alin.serg.as.as@gmail.com>
2021-08-31 23:32:11 +0700

    some

--- components/Data_Preprocessor.py
+++ components/Data_Preprocessor.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 import albumentations as A
 from PIL import Image
 
-project_directory = '/home/alina/PycharmProjects/roads/'
+project_directory = '/home/alina/PycharmProjects/roads_git/'
 
 video_path = project_directory + '/video/video/video_0_00:00:16.mp4'
 save_video_cut_directory = project_directory + 'video/cut/'
 save_storyboards_directory = project_directory + 'video/storyboards/'
-
 image_directory = project_directory + 'imgs/Road_defects/PNG/FULL_SIZE/def_imgs/'
 mask_directory = project_directory + 'imgs/Road_defects/PNG/FULL_SIZE/def_masks/'
 save_image_directory = project_directory + '/imgs/Road_defects/PNG/imgs/'
